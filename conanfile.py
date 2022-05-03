879f9999cdc1c0531c1db75e41896fe37b26b6bf
Mark Barbaric <mark.barbaric@mirriad.com>
2022-05-03 16:02:41 +0100

    Windows fixes.

--- conanfile.py
+++ conanfile.py
@@ -4,10 +4,11 @@ class OpenCVConanFile(ConanFile):
 
     settings = 'os', 'os_build', 'compiler', 'build_type', 'arch', 'arch_build'
 
-    requires = ['opencv/4.5.2']
-
     default_options = {
         'libpng:shared' : True
     }
 
     generators = 'cmake', 'cmake_find_package', 'cmake_paths', 'txt'
+
+    def requirements(self):
+        self.requires('opencv/4.5.2')
