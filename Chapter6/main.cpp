abd3096b23d46050a22922134116527824954bb9
Mark Barbaric <mark.barbaric@mirriad.com>
2022-05-30 14:14:25 +0100

    Tidied up code in main.cpp.

--- Chapter6/main.cpp
+++ Chapter6/main.cpp
@@ -7,7 +7,6 @@
 #include <opencv2/ml.hpp>
 #include <opencv2/imgproc.hpp>
 
-std::shared_ptr<OpenCVHelpers::MultipleImageWindow> miw;
 cv::Ptr<cv::ml::SVM> svm;
 cv::Scalar green(0, 255, 0), blue (255, 0, 0), red(0, 0, 255);
 
@@ -16,7 +15,7 @@ const char* keys = {
 "{@image || Image to classify}"
 };
 
-std::vector<std::vector<float>> ExtractFeatures(cv::Mat image, std::vector<int>* left = nullptr, std::vector<int>* top = nullptr){
+std::vector<std::vector<float>> ExtractFeatures(const std::weak_ptr<OpenCVHelpers::MultipleImageWindow>& sharedWindow, cv::Mat image, std::vector<int>* left = nullptr, std::vector<int>* top = nullptr){
     std::vector<std::vector<float>> output;
     std::vector<std::vector<cv::Point>> contours;
     cv::Mat input = image.clone();
@@ -30,6 +29,8 @@ std::vector<std::vector<float>> ExtractFeatures(cv::Mat image, std::vector<int>*
     }
 
     cv::RNG rng(0XFFFFFFFF);
+
+    std::weak_ptr<OpenCVHelpers::MultipleImageWindow> multipleIMageWindow;
 
     for(int i = 0; i < contours.size(); ++i){
         cv::Mat mask = cv::Mat::zeros(input.rows, input.cols, CV_8UC1);
@@ -57,16 +58,19 @@ std::vector<std::vector<float>> ExtractFeatures(cv::Mat image, std::vector<int>*
                 top->push_back(static_cast<int>(r.center.y));
             }
 
-            miw->addImage("Extract Features", mask*255);
-            miw->render();
-            cv::waitKey(10);
+            if(const auto tempWindow = sharedWindow.lock()) {
+                tempWindow->addImage("Extract Features", mask * 255);
+                tempWindow->render();
+                cv::waitKey(10);
+            }
         }
 
     }
     return output;
 }
 
-bool readFolderAndExtractFeatures(std::string folder, int label, int num_for_test,
+bool readFolderAndExtractFeatures(const std::weak_ptr<OpenCVHelpers::MultipleImageWindow>& sharedWindow,
+                                  std::string folder, int label, int num_for_test,
                                   std::vector<float>& trainingData, std::vector<int>& responsesData,
                                   std::vector<float>& testData, std::vector<float>& testResponsesData){
     cv::VideoCapture images;
@@ -83,7 +87,7 @@ bool readFolderAndExtractFeatures(std::string folder, int label, int num_for_tes
     while(images.read(frame)){
         const auto preprocessedImage = ObjectDetection::Preprocessing::Preprocess(frame);
 
-        const auto features = ExtractFeatures(preprocessedImage);
+        const auto features = ExtractFeatures(sharedWindow, preprocessedImage);
 
         for(int i = 0; i < features.size(); ++i){
             if(img_index >= num_for_test){
@@ -103,7 +107,8 @@ bool readFolderAndExtractFeatures(std::string folder, int label, int num_for_tes
     return true;
 }
 
-void plotTrainData(cv::Mat trainData, cv::Mat labels, float *error=NULL)
+void plotTrainData(const std::weak_ptr<OpenCVHelpers::MultipleImageWindow>& sharedWindow,
+                   cv::Mat trainData, cv::Mat labels, float *error = nullptr)
 {
     float area_max, ar_max, area_min, ar_min;
     area_max= ar_max= 0;
@@ -137,25 +142,32 @@ void plotTrainData(cv::Mat trainData, cv::Mat labels, float *error=NULL)
         int label= labels.at<int>(i);
         // Set color depend of label
         cv::Scalar color;
-        if(label==0)
-            color= green; // NUT
-            else if(label==1)
-                color= blue; // RING
-                else if(label==2)
-                    color= red; // SCREW
 
-                    circle( plot, cv::Point(x, y),   3,  color, -1, 8);
+        if(label==0) {
+            color = green;
+        }// NUT
+        else if(label==1) {
+            color = blue; // RING
+        }
+        else if(label==2) {
+            color = red; // SCREW
+            circle(plot, cv::Point(x, y), 3, color, -1, 8);
+        }
     }
 
-    if(error!=NULL){
+    if(error){
         std::stringstream ss;
         ss << "Error: " << *error << '\%';
         putText(plot, ss.str().c_str(), cv::Point(20,512-40), cv::FONT_HERSHEY_SIMPLEX, 0.75, cv::Scalar(200,200,200), 1, cv::LINE_AA);
     }
-    miw->addImage("Plot", plot);
+
+    if(const auto tempWindow = sharedWindow.lock()){
+        tempWindow->addImage("Plot", plot);
+    }
 }
 
-void trainAndTest(){
+void trainAndTest(cv::Ptr<cv::ml::SVM>& svm,
+        const std::weak_ptr<OpenCVHelpers::MultipleImageWindow>& window){
 
     std::vector<float> trainingData;
     std::vector<int> responsesData;
@@ -165,11 +177,11 @@ void trainAndTest(){
     int num_for_test = 20;
 
     // Get the nut images
-    readFolderAndExtractFeatures("../TrainingData/nut/tuerca_%04d.pgm", 0, num_for_test, trainingData, responsesData, testData, testResponsesData);
+    readFolderAndExtractFeatures(window, "../TrainingData/nut/tuerca_%04d.pgm", 0, num_for_test, trainingData, responsesData, testData, testResponsesData);
     // Get and process the ring images
-    readFolderAndExtractFeatures("../TrainingData/ring/arandela_%04d.pgm", 1, num_for_test, trainingData, responsesData, testData, testResponsesData);
+    readFolderAndExtractFeatures(window, "../TrainingData/ring/arandela_%04d.pgm", 1, num_for_test, trainingData, responsesData, testData, testResponsesData);
     // get and process the screw images
-    readFolderAndExtractFeatures("../TrainingData/screw/tornillo_%04d.pgm", 2, num_for_test, trainingData, responsesData, testData, testResponsesData);
+    readFolderAndExtractFeatures(window, "../TrainingData/screw/tornillo_%04d.pgm", 2, num_for_test, trainingData, responsesData, testData, testResponsesData);
 
     std::cout << "Num of train samples: " << responsesData.size() << ".\n";
     std::cout << "Num of test samples: " << testResponsesData.size() << ".\n";
@@ -182,8 +194,6 @@ void trainAndTest(){
     cv::Mat testResponses(testResponsesData.size(), 1, CV_32FC1, &testResponsesData[0]);
 
     cv::Ptr<cv::ml::TrainData> tdata= cv::ml::TrainData::create(trainingDataMat, cv::ml::ROW_SAMPLE, responses);
-
-    svm = cv::ml::SVM::create();
     svm->setType(cv::ml::SVM::C_SVC);
     svm->setNu(0.05);
     svm->setKernel(cv::ml::SVM::CHI2);
@@ -192,7 +202,7 @@ void trainAndTest(){
     svm->setTermCriteria(cv::TermCriteria(cv::TermCriteria::MAX_ITER, 100, 1e-6));
     svm->train(tdata);
 
-    if(testResponsesData.empty()){
+    if(!testResponsesData.empty()){
         std::cout << "Evaluation" << std::endl;
         std::cout << "==========" << std::endl;
         // Test the ML Model
@@ -204,12 +214,11 @@ void trainAndTest(){
         float error= 100.0f * countNonZero(errorMat) / testResponsesData.size();
         std::cout << "Error: " << error << "%" << std::endl;
         // Plot training data with error label
-        plotTrainData(trainingDataMat, responses, &error);
+        plotTrainData(window, trainingDataMat, responses, &error);
 
     }else{
-        plotTrainData(trainingDataMat, responses);
+        plotTrainData(window, trainingDataMat, responses);
     }
-
 }
 
 int main(int argc, char* argv[]){
@@ -227,10 +236,7 @@ int main(int argc, char* argv[]){
         return 1;
     }
 
-    cv::String inputImageFilePath = cli.get<cv::String>(0);
-
-    miw = std::make_shared<OpenCVHelpers::MultipleImageWindow>("Main window", cv::WINDOW_AUTOSIZE);
-
+    const auto inputImageFilePath = cli.get<cv::String>(0);
     cv::Mat inputImage = cv::imread(inputImageFilePath, 0);
 
     if(inputImage.data == nullptr){
@@ -241,12 +247,15 @@ int main(int argc, char* argv[]){
     cv::Mat inputImageClone = inputImage.clone();
     cv::cvtColor(inputImageClone, inputImageClone, cv::COLOR_RGB2GRAY);
 
-    trainAndTest();
+    const auto miw = std::make_shared<OpenCVHelpers::MultipleImageWindow>("Main window", cv::WINDOW_AUTOSIZE);
+
+    cv::Ptr<cv::ml::SVM> svm;
+    trainAndTest(svm, miw);
 
     cv::Mat pre = ObjectDetection::Preprocessing::Preprocess(inputImageClone);
 
     std::vector<int> posLeft, posTop;
-    std::vector<std::vector<float>> features = ExtractFeatures(pre, &posLeft, &posTop);
+    std::vector<std::vector<float>> features = ExtractFeatures(miw, pre, &posLeft, &posTop);
 
     if(!features.empty()){
         std::cout << "Failed to extract features from source image \n.";
